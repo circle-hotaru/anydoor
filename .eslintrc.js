07c5ddeee5b6c1293268ef5172c2d51dbf3043d5
circlehotarux <mightyherox@gmail.com>
2021-01-30 17:07:32 +0800

    complete

---
+++ .eslintrc.js
@@ -0,0 +1,18 @@
+module.exports = {
+    "extends": "eslint:recommended",
+    "rules": {
+        "no-console": ["error", {
+            "allow": ["warn", "error", "info"]
+        }]
+    },
+    "parser": "babel-eslint",
+    "parserOptions": {
+        "ecmaVersion": 6,
+        "sourceType": "script"
+    },
+    "env": {
+        "node": true,
+        "es6": true,
+        "mocha": true
+    }
+};
--- README.md
+++ README.md
@@ -1,2 +1,26 @@
 # anydoor
 Tiny NodeJS Static Web Server
+
+## 安装
+
+```
+npm i -g anydoor
+```
+
+## 使用方法
+
+```
+anydoor # 把当前文件夹作为静态资源服务器根目录
+
+anydoor -p 8080 # 设置端口号为8080
+
+anydoor -h localhost # 设置 host 为 localhost
+
+anydoor -d /usr # 设置根目录为 /usr
+```
+
+
+
+## 注释
+
+学习 NodeJS 课程项目，原项目：[Samaritan89 / anydoor](https://github.com/Samaritan89/anydoor)
